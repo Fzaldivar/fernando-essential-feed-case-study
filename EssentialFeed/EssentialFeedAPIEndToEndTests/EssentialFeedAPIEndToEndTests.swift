d1e4cabf05285d00f700795c1bd9b61ae2e45b1b
Fernando Zaldívar Coto <fernandozaldivarcoto@gmail.com>
2023-10-20 15:59:45 -0600

    Add end-to-end API tests in a separate test target

---
+++ EssentialFeed/EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
@@ -0,0 +1,13 @@
+//
+//  EssentialFeedAPIEndToEndTests.swift
+//  EssentialFeedAPIEndToEndTests
+//
+//  Created by Fernando Zaldivar on 20/10/23.
+//
+
+import XCTest
+
+final class EssentialFeedAPIEndToEndTests: XCTestCase {
+
+
+}
