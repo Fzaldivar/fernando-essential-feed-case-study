b11a0f4162472c5349fe0beeb0e67a371598db4c
Fernando Zaldívar Coto <fernandozaldivarcoto@gmail.com>
2023-12-06 23:06:41 -0600

    Move FeedErrorViewModel to separate file

---
+++ EssentialFeed/EssentialFeediOS/Feed UI/Feed Presentation/FeedErrorViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedErrorViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by Fernando Zaldivar on 6/12/23.
+//
+
+import Foundation
+
+struct FeedErrorViewModel {
+    let message: String?
+}
